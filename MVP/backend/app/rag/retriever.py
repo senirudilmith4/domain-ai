e0a649289361d7a024bfe11b854749e4a87bc483
senirudilmith4 <dilmith.seniru0@gmail.com>
2026-02-04 16:09:49 +0530

    Logic for calling LLM & RAG

---
+++ MVP/backend/app/rag/retriever.py
@@ -0,0 +1,9 @@
+def retrieve_context(query: str, top_k: int):
+    # TEMP MOCK (replace with ChromaDB later)
+    return [
+        {
+            "content": "Course registration closes on March 10.",
+            "source": "Student Handbook 2024",
+            "page": 12
+        }
+    ]
