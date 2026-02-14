8e3313ee662337732fe590f1f54175b83c9be809
senirudilmith4 <dilmith.seniru0@gmail.com>
2026-02-14 18:07:55 +0530

    RAG data structure implementation

--- MVP/backend/pipeline/rag_pipeline.py
+++ MVP/backend/pipeline/rag_pipeline.py
@@ -6,8 +6,8 @@ from typing import List, Dict, Any
 from MVP.backend.utils.logger import get_logger
 from MVP.backend.ingestion.load_docs import load_documents
 from MVP.backend.ingestion.chunk_docs import chunk_documents
-from MVP.backend.vector_store.embedder import embed_documents
-from MVP.backend.vector_store.chroma_client import ChromaVectorStore
+from MVP.backend.chroma_db.embedder import embed_documents
+from MVP.backend.chroma_db.vector_db import ChromaVectorStore
 
 logger = get_logger(__name__)
 
